0d849a8d098d87277b48e9e245514db444bc1783
tasfia236 <tasfiatabassum300@gmail.com>
2024-11-26 19:36:56 +0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navber from "@/components/Navber";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="relative w-full flex items-center justify-center">
+          <Navber></Navber>
+        </div>
         {children}
       </body>
     </html>
